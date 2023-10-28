678f8dd0279902987f2b6f79a2e88c13100eb976
kyoungik lee <kyoungik.lee@innotree.com>
2023-10-28 16:06:44 +0900

    Dish에 Type enum 추가

--- src/chapter5/Mapping.java
+++ src/chapter5/Mapping.java
@@ -7,11 +7,25 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
+
 public class Mapping {
+
+    public static final List<Dish> menu = asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
     public static void main(String[] args) {
 
         //메뉴명을 출력하시오
-        Dish.menu.stream().map(new Function<Dish, String>() {
+        menu.stream().map(new Function<Dish, String>() {
             @Override
             public String apply(Dish dish) {
                 return dish.getName();
