44e9324abfab4bd18417e4d54a18f58bc0713ec9
roadseeker <kyoungik.lee@gmail.com>
2023-10-30 11:09:44 +0900

    reducing 학습
    - Collectors.reducing 사용법
    - reducing을 사용하지 않고 합계 구하는 방법(map, reduce)
    - mapToIng(), sum()을 이용하는 방법

---
+++ test/chapter6/ReducingTest.java
@@ -0,0 +1,35 @@
+package chapter6;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReducingTest {
+
+    @Test
+    @DisplayName(value = "reducing() 이용하여 메뉴의 칼로리 총합을 구한다")
+    void calculateTotalCaloriesTest() {
+        assertThat(Reducing.calculateTotalCalories()).isEqualTo(4300);
+    }
+
+    @Test
+    @DisplayName(value = "reducing()에서 메소드 레퍼런스를 사용하여 메뉴 칼로리 총합을 구한다")
+    void calculateTotalCaloriesWithMethodReferenceUsingReducing() {
+        assertThat(Reducing.calculateTotalCaloriesWithMethodReferenceUsingReducing()).isEqualTo(4300);
+
+    }
+
+    @Test
+    @DisplayName(value = "reducing() 메소드 사용하지 않고 메뉴의 칼로리 총합을 구한다")
+    void calculateTotalCaloriesWithMethodReferenceTest() {
+        assertThat(Reducing.calculateTotalCaloriesWithoutCollectors()).isEqualTo(4300);
+
+    }
+
+    @Test
+    @DisplayName(value = "Sum 메소드를 이용하여 합계 구하기")
+    void calculateTotalCaloriesUsingSum() {
+        assertThat(Reducing.calculateTotalCaloriesUsingSum()).isEqualTo(4300);
+    }
+}
