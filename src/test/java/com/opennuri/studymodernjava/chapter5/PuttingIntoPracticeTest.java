1ae10d1d79016b86fffb4fe4b3d202b16d3f5f85
roadseeker <kyoungik.lee@gmail.com>
2023-11-03 10:48:04 +0900

    PuttingIntoPractice 테스트 코드 작성

--- src/test/java/com/opennuri/studymodernjava/chapter5/PuttingIntoPracticeTest.java
+++ src/test/java/com/opennuri/studymodernjava/chapter5/PuttingIntoPracticeTest.java
@@ -1,8 +1,13 @@
 package com.opennuri.studymodernjava.chapter5;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.convert.DataSizeUnit;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,44 +17,139 @@ class PuttingIntoPracticeTest {
     @Test
     @DisplayName(value = "거래내역 중 최소값 확인")
     void findMinValueInAllTransactionsUsingComparing() {
-        assertThat(PuttingIntoPractice.findMinValueInAllTransactionsUsingComparing().getValue())
-                .isEqualTo(300);
+
+        assertThat(PuttingIntoPractice.findMinValueInAllTransactionsUsingComparing())
+                .isNotNull()
+                .extracting("year","value" )
+                .contains(2011, 300);
     }
 
     @Test
-    @DisplayName(value = "거래자가 거주하는 도시")
+    @DisplayName(value = "거래자가 거주하는 도시를 유일한 값으로 확인")
+    void findAllUniqueCitiesWhereTradersWork() {
+        assertThat(PuttingIntoPractice.findAllUniqueCitiesWhereTradersWork())
+                .isNotNull()
+                .hasSize(2)
+                .contains("Cambridge", "Milan" );
+    }
+
+    @Test
+    @DisplayName(value = "Cambridge에 사는 거래자의 모든 거래내역 출력")
+    void findAllTransactionsOfTradersInCambridge() {
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+         List<Transaction> transactions = Arrays.asList(
+                 new Transaction(alan, 2012, 950),
+                 new Transaction(brian, 2010, 400),
+                 new Transaction(brian, 2011, 300),
+                 new Transaction(raoul, 2012, 1000),
+                 new Transaction(raoul, 2011, 400)
+
+        );
+
+        List<Transaction> allTransactionsOfTradersInCambridge
+                = PuttingIntoPractice.findAllTransactionsOfTradersInCambridge();
+        assertThat(allTransactionsOfTradersInCambridge)
+                .isNotNull()
+                .hasSize(5)
+                .containsExactlyInAnyOrderElementsOf(transactions);
+    }
+
+    @Test
     void findMinValueInAllTransactions() {
+        Transaction minValueInAllTransactions = PuttingIntoPractice.findMinValueInAllTransactions();
+        assertThat(minValueInAllTransactions)
+                .isNotNull()
+                .extracting("year", "value")
+                .contains(2011, 300);
+
     }
 
     @Test
     void findMaxValueInAllTransactionsUsingReduce() {
+        int maxValueInAllTransactionsUsingReduce
+                = PuttingIntoPractice.findMaxValueInAllTransactionsUsingReduce();
+
+        assertThat(maxValueInAllTransactionsUsingReduce).isEqualTo(1000);
     }
 
     @Test
+    @DisplayName(value = "모든 거래에서 최고값 찾기")
     void findMaxValueInAllTransactions() {
+
+        int maxValueInAllTransactions
+                = PuttingIntoPractice.findMaxValueInAllTransactions();
+
+        assertThat(maxValueInAllTransactions)
+                .isEqualTo(1000);
     }
 
-    @Test
-    void findAllTransactionsOfTradersInCambridge() {
-    }
 
     @Test
+    @DisplayName(value = "Milan에 거주하는 거래자가 있는가?")
     void isAnyTradersBasedInMilan() {
+        assertThat(PuttingIntoPractice.isAnyTradersBasedInMilan()).isTrue();
     }
 
     @Test
     void findStringOfNamesAllTradersSortedAlphabetically() {
+        List<String> stringOfNamesAllTraders
+                = PuttingIntoPractice.findStringOfNamesAllTradersSortedAlphabetically();
+
+        assertThat(stringOfNamesAllTraders)
+                .isNotNull()
+                .hasSize(4)
+                .contains("Raoul", "Mario", "Alan", "Brian")
+                ;
+
     }
 
     @Test
+    @DisplayName(value = "Cambridge에 거주하는 모든 거래자 이름을 찾아 정렬")
     void findAllTradersInCambridgeAndSortByname() {
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+
+        assertThat(PuttingIntoPractice.findAllTradersInCambridgeAndSortByname())
+                .isNotNull()
+                .hasSize(3)
+                .containsExactlyInAnyOrder(raoul, alan, brian);
     }
 
     @Test
-    void findAllUniqueCitiesWhereTradersWork() {
-    }
-
-    @Test
+    @DisplayName(value = "2011년부터 발생한 모든 거래를 찾아 값으로 정렬(작은 값에서 큰 값)")
     void findTransactionsByYear() {
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        assertThat(PuttingIntoPractice.findTransactionsByYear())
+                .isNotNull()
+                .hasSize(6)
+                .extracting("year", "value")
+                .contains(
+                        Tuple.tuple(2011, 300),
+                        Tuple.tuple(2012, 950)
+                )
+        ;
+
     }
 }
