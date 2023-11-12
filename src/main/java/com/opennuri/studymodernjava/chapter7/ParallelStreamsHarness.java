fbde393e172974c6be17c529f55b55f289371238
kyoungiklee <kyoungik.lee@gmail.com>
2023-11-12 19:51:35 +0900

    Parallel sum 성능 확인 코드 추가

--- src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
+++ src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
@@ -8,6 +8,7 @@ public class ParallelStreamsHarness {
                 + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + "msecs");
         System.out.println("Sequantial Sum done in: "
                 + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + "msecs");
+        System.out.println("Parallel Sum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + "msecs");
     }
 
     private static <T, R> long measurePerf(Function<T, R> f, T input) {
