24913282d87e54e2a65c3d306837ac8e168d8cec
kyoungiklee <kyoungik.lee@gmail.com>
2023-11-12 21:53:55 +0900

    ranged sum 성능 확인 추가

--- src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
+++ src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
@@ -10,6 +10,10 @@ public class ParallelStreamsHarness {
                 + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + "msecs");
         System.out.println("Parallel Sum done in: "
                 + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + "msecs");
+        System.out.println("Ranged Sum done in: "
+                + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + "msecs");
+        System.out.println("Parallel Sum done in: "
+                + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + "msecs");
         System.out.println("Parallel ranged Sum done in: "
                 + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + "msecs");
         System.out.println("SideEffect Sum done in: "
