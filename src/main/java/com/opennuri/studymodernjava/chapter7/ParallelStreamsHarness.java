f1c31bf3d1085ea8a32fe71b323c3558e732fda2
kyoungiklee <kyoungik.lee@gmail.com>
2023-11-12 15:22:01 +0900

    parallel stream 학습

---
+++ src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
@@ -0,0 +1,24 @@
+package com.opennuri.studymodernjava.chapter7;
+
+import java.util.function.Function;
+
+public class ParallelStreamsHarness {
+    public static void main(String[] args) {
+        System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + "msecs");
+    }
+
+    private static <T, R> long measurePerf(Function<T, R> f, T input) {
+        long fastest = Long.MAX_VALUE;
+
+        for(int i =0; i < 10; i++) {
+            long start = System.nanoTime();
+            R result = f.apply(input);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Resutl: " + result);
+            if (duration < fastest) {
+                fastest = duration;
+            }
+        }
+        return fastest;
+    }
+}
