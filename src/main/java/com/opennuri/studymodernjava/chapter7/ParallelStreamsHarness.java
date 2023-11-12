b241f9c0947cb2d87ae2b85db4f65e82a5373fbc
kyoungiklee <kyoungik.lee@gmail.com>
2023-11-12 20:29:25 +0900

    rangedSum 성능 확인 코드 추가

--- src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
+++ src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreamsHarness.java
@@ -8,7 +8,10 @@ public class ParallelStreamsHarness {
                 + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + "msecs");
         System.out.println("Sequantial Sum done in: "
                 + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + "msecs");
-        System.out.println("Parallel Sum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + "msecs");
+        System.out.println("Parallel Sum done in: "
+                + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + "msecs");
+        System.out.println("Ranged Sum done in: "
+                + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + "msecs");
     }
 
     private static <T, R> long measurePerf(Function<T, R> f, T input) {
