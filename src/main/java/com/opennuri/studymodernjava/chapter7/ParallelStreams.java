9db531d56986dbabf075fe91e8f2800af75fe027
kyoungiklee <kyoungik.lee@gmail.com>
2023-11-12 17:20:02 +0900

    sequentialSum 성능 확인

--- src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreams.java
+++ src/main/java/com/opennuri/studymodernjava/chapter7/ParallelStreams.java
@@ -1,12 +1,19 @@
 package com.opennuri.studymodernjava.chapter7;
 
+import java.util.stream.Stream;
+
 public class ParallelStreams {
 
     public static long iterativeSum(long n) {
         long result = 0;
-        for (long i = 0;  i < n; i++) {
+        for (long i = 0;  i <= n; i++) {
             result += i;
         }
         return result;
+    }
+
+    public static long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1).limit(n)
+                .reduce(Long::sum).orElse(0L);
     }
 }
